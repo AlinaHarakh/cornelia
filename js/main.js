35cfd98470d865ee98cd6b0fb2520288695d8844
Harakh <106341772+AlinaHarakh@users.noreply.github.com>
2022-09-05 18:42:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				animItem.classList.remove('_active');
+			}
+		}
+	}
+
+	function offset(el) {
+		var rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	setTimeout(() => {
+		animOnScroll();
+	}, 500);
+
+}
+$('.burger').on('click', function () {
+	$('.menu__list').toggleClass('menu__list--active');
+	$(this).toggleClass("open");
+});
+
+
+
+
+// $(function () {
+
+// })
